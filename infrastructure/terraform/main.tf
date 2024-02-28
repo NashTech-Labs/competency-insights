5b52bac96a5203cdfc1a2c22efac2804ff464c8b
abidknashtech <abid.khan@nashtechglobal.com>
2024-02-28 16:56:00 +0530

    created gcp infa through terraform

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,36 @@
+#Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 4
+}
+
+#-----------------------pub sub--------------------------------
+resource "google_pubsub_topic" "nasher-info" {
+  name = "nasher.info"
+}
+
+#-----------------------GKE Cluster for applications----------------------------
+resource "google_container_cluster" "competency-insights-cluster" {
+  name     = "competency-insights-cluster"
+  location = var.gcp_region_1
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = ""
+    services_ipv4_cidr_block = ""
+  }
+  enable_autopilot = true
+  deletion_protection = false
+}
+
+#----------------------GCP firestore----------------------------
+resource "google_project_service" "firestore" {
+  project = var.app_project
+  service = "firestore.googleapis.com"
+}
+
+resource "google_firestore_database" "firestore_db" {
+  project     = var.app_project
+  name        = "competency-insights"
+  location_id = var.gcp_region_1
+  type        = "FIRESTORE_NATIVE"
+  depends_on = [google_project_service.firestore]
+}
+
---
+++ infrastructure/terraform/terraform.tfvars
@@ -0,0 +1,9 @@
+# Application Definition 
+app_name        = "competency-insights"
+app_environment = "dev"
+app_project     = "sonarqube-289802"
+
+# GCP Settings
+gcp_region_1  = "us-east1"
+gcp_zone_1    = "us-east1-b"
+gcp_auth_file = "key.json"
