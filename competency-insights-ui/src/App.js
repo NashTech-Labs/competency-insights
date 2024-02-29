c749165a7b258e2e13705125eb2c3b707e31b4bb
DeepaMittal <deepamittalmca@gmail.com>
2024-03-01 04:58:42 +0530

    added login page

--- competency-insights-ui/src/App.js
+++ competency-insights-ui/src/App.js
@@ -1,38 +1,13 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
-import { useMsal, useMsalAuthentication } from "@azure/msal-react";
-import { InteractionType } from "@azure/msal-browser";
+import {AppRouters} from "./routes/AppRouters";
 
 function App() {
-  useMsalAuthentication(InteractionType.Redirect);
-  const [m_strUser, setm_strUser] = useState("");
-
-  const { instance ,accounts } = useMsal();
-
-  useEffect(() => {
-    try {
-      const username = accounts[0].username;
-      setm_strUser(username);
-    } catch (e) {
-      console.error("Error while fetching username:", e);
-    }
-  }, [accounts]);
-
-  const handleLogout = () => {
-    instance.logoutPopup();
-  };
-
-
-  if (m_strUser !== "") {
-    return (
-        <div className="App">
-          <div>User: {m_strUser}</div>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-    );
-  } else {
-    return <div>Please wait...</div>;
-  }
+  return (
+      <div className="App">
+        <AppRouters />
+      </div>
+  );
 }
 
 export default App;
+
