05bbb030f765d7f774d55ef25302adf0348a8907
DeepaMittal <deepamittalmca@gmail.com>
2024-03-01 04:02:18 +0530

    handle login

--- competency-insights-ui/src/App.js
+++ competency-insights-ui/src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
-import Login from './pages/Login';
+import React, { useState, useEffect } from "react";
+import { useMsal, useMsalAuthentication } from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
 
 function App() {
-  return ( <Login />);
+  useMsalAuthentication(InteractionType.Redirect);
+  const [m_strUser, setm_strUser] = useState("");
+
+  const { instance ,accounts } = useMsal();
+
+  useEffect(() => {
+    try {
+      const username = accounts[0].username;
+      setm_strUser(username);
+    } catch (e) {
+      console.error("Error while fetching username:", e);
+    }
+  }, [accounts]);
+
+  const handleLogout = () => {
+    instance.logoutPopup();
+  };
+
+
+  if (m_strUser !== "") {
+    return (
+        <div className="App">
+          <div>User: {m_strUser}</div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+  } else {
+    return <div>Please wait...</div>;
+  }
 }
 
 export default App;
--- competency-insights-ui/src/index.js
+++ competency-insights-ui/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MsalProvider } from '@azure/msal-react';
+import {PublicClientApplication} from "@azure/msal-browser";
+import {msalConfig} from "./auth/authConfig";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const msalInstance = new PublicClientApplication(msalConfig);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+      <MsalProvider instance={msalInstance}>
+      <App />
+      </MsalProvider>
   </React.StrictMode>
 );
