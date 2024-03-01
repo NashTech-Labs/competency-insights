c6373de5cd30ac16f3c691cb2ddbcb42fdb87c7c
abidknashtech <127501072+abidknashtech@users.noreply.github.com>
2024-03-01 14:26:32 +0530

    Update apps-deployment-script.sh

--- apps-deployment-script.sh
+++ apps-deployment-script.sh
@@ -52,7 +52,7 @@ else
 fi
 
 # Tag Docker image correctly
-docker tag ${GCR_REPOSITORY}/$SERVICE_NAME:latest
+docker tag ${GCR_REPOSITORY}/$SERVICE_NAME:latest ${GCR_REPOSITORY}/$SERVICE_NAME:latest
 
 # Push Docker image to GCR (Artifact Registry)
 docker push ${GCR_REPOSITORY}/$SERVICE_NAME:latest
