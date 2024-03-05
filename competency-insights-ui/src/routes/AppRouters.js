3af4bb7ef5986c9524d7c2b634b28643e888c6ec
sabiaparveen1 <sabia.parveen@nashtechglobal.com>
2024-03-05 16:07:03 +0530

    commented coz there was no Dashboard file

--- competency-insights-ui/src/routes/AppRouters.js
+++ competency-insights-ui/src/routes/AppRouters.js
@@ -1,15 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../auth/Login";
-import Dashboard from "../auth/Dashboard";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Dashboard from "../auth/Dashboard";
+
 import {Login, ProfilePage} from "../pages";
 export const AppRouters = () => {
     return (
         <Routes>
             <Route>
                 <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                {/* <Route path="/dashboard" element={<Dashboard />} /> */}
                 <Route path="/profile" element={<ProfilePage />} />
             </Route>
         </Routes>
