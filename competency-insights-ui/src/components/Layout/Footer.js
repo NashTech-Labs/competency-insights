fb0d94c6091a177807800fe20ed9e052cd200604
sabiaparveen1 <sabia.parveen@nashtechglobal.com>
2024-03-05 15:48:24 +0530

    added some theme features

--- competency-insights-ui/src/components/Layout/Footer.js
+++ competency-insights-ui/src/components/Layout/Footer.js
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
     return (
         <footer>
-            <div  className="bg-white shadow dark:bg-gray-800">
-            <div className="p-4 mx-auto max-w-screen-xl md:flex md:items-center md:justify-between md:p-6">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="/" className="hover:underline">Knashtech OKR</Link></span>
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="/" className="hover:underline">Terms and Conditions</Link></span>
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="https://nashtechglobal.go1percent.com/" className="hover:underline" target="_blank">Go1%</Link></span>
+            <div className="bg-white shadow dark:bg-gray-900">
+                <div className="p-4 mx-auto max-w-screen-xl md:flex md:items-center md:justify-between md:p-6">
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="/" className="hover:underline">Nashtech OKR's</Link></span>
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="/" className="hover:underline">Terms and Conditions</Link></span>
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="https://nashtechglobal.go1percent.com/" className="hover:underline" target="_blank">Go1%</Link></span>
 
-            </div>
+                </div>
             </div>
         </footer>
     )
 }
+export default Footer;
--- competency-insights-ui/src/routes/AppRouters.js
+++ competency-insights-ui/src/routes/AppRouters.js
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../auth/Login";
 import Dashboard from "../auth/Dashboard";
 
 export const AppRouters = () => {
     return (
-        <Router>
-            <Routes>
+        <Routes>
+            <Route>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </Router>
+            </Route>
+        </Routes>
     )
 }
