014e751e88c00990927b7d6627ffc1ce671db80b
Rahulkrsinhaa <rahul.sinha@nashtechglobal.com>
2024-03-06 11:41:43 +0530

    added nav bar component

--- competency-insights-ui/src/routes/AppRouters.js
+++ competency-insights-ui/src/routes/AppRouters.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from '../components/Layout/NavBar';
 import {Login, ProfilePage} from "../pages";
 export const AppRouters = () => {
     return (
         <Router>
+              <Navbar/>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/profile" element={<ProfilePage />} />
