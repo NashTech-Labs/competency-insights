1222813f7c88f36b56c9da0dc7ad56a43554618c
sahil babbar <sahil.babbar@nashtechglobal.com>
2024-03-07 10:05:45 +0530

    intial commit for profile page

--- competency-insights-ui/src/routes/AppRouters.js
+++ competency-insights-ui/src/routes/AppRouters.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import {Login, ProfilePage} from "../pages";
+import {Login} from "../pages";
+import ProfileDetails from "../pages/Profile/ProfileDetails";
 export const AppRouters = () => {
     return (
         <Routes>
             <Route>
                 <Route path="/" element={<Login />} />
-                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/profile" element={<ProfileDetails />} />
             </Route>
         </Routes>
     )
