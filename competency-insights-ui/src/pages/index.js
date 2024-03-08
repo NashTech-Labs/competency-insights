80fc0432ad6dc7c8c819db43cbd23451c2d672f7
Anshika1603 <anshika.srivastava@knoldus.com>
2024-03-08 10:53:20 +0530

    Added CreateOkr Page

--- competency-insights-ui/src/pages/index.js
+++ competency-insights-ui/src/pages/index.js
@@ -1,3 +1,4 @@
 export { Login} from "./Login";
 export {ProfileDetails} from "./Profile/ProfileDetails";
 export {TeamPage} from "./Team/TeamPage";
+export {CreateOkrPage} from "./CreateOkr/CreateOkrPage";
