0551ad8dab7b0e2d359938a52d870b65607cee0e
abidknashtech <abid.khan@nashtechglobal.com>
2024-03-08 14:24:56 +0530

    Added Oauth2 security with Azure directory

--- contribution-service/src/main/java/com/nashtech/contributionservice/rest/NasherController.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/rest/NasherController.java
@@ -5,6 +5,7 @@ import com.nashtech.contributionservice.service.PasGo1Service;
 import com.nashtech.contributionservice.service.Processor;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,24 +24,28 @@ public class NasherController {
     private final Processor processor;
     private final PasGo1Service pasGo1Service;
 
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
     @GetMapping("nashers")
     public Flux<Nasher> getAllNasher() {
         log.info("Enter into NasherController: Get all Nashers request");
         return processor.getNashers();
     }
 
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
     @GetMapping("nasher/{empId}")
     public Mono<Nasher> getNasherById(@PathVariable String empId) {
         log.info("Enter into NasherController: Get Nasher by employee Id: {}",empId);
         return processor.getNasherInfo(empId);
     }
 
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
     @PostMapping("nasher/save") // Temp basis to check firestore insertion
     public void saveNasher(@RequestBody Nasher nasher) {
         log.info("Enter into NasherController: Saving Nasher with Employee Id: {} ", nasher.getEmpId());
         processor.saveNasher(nasher);
     }
 
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
     @GetMapping("trigger/pas")
     public String triggerPasService() {
         log.info("Enter into NasherController: Processing PAS request");
@@ -48,6 +53,7 @@ public class NasherController {
         return "Successfully triggered Pas service";
     }
 
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
     @GetMapping("trigger/go1")
     public String triggerGo1Service() {
         log.info("Enter into NasherController: Processing GO1% request");
