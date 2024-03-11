70efefcce92cea91a498dc869e3dbb6e403a090e
Sushant Gupta <Sushant.Gupta@nashtechglobal.com>
2024-03-12 00:02:37 +0530

    service account

--- contribution-service/src/main/java/com/nashtech/contributionservice/controller/NasherController.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/controller/NasherController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-
 @RestController
 @RequestMapping("/cs")
 @AllArgsConstructor
