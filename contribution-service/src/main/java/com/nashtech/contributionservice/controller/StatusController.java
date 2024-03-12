eeb4f58c516fec7235db89f216dcb2c436a14238
Sushant Gupta <Sushant.Gupta@nashtechglobal.com>
2024-03-12 14:05:28 +0530

    service account default

--- contribution-service/src/main/java/com/nashtech/contributionservice/controller/StatusController.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/controller/StatusController.java
@@ -3,7 +3,7 @@ package com.nashtech.contributionservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/")
 @Slf4j
