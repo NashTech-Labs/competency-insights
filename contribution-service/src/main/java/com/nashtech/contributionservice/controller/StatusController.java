eda57d9bceea525f05946bb2e01870d2c51a8f83
Sushant Gupta <Sushant.Gupta@nashtechglobal.com>
2024-03-12 20:58:08 +0530

    service account default

--- contribution-service/src/main/java/com/nashtech/contributionservice/controller/StatusController.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/controller/StatusController.java
@@ -1,25 +1,27 @@
 package com.nashtech.contributionservice.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/")
-@Slf4j
 public class StatusController {
 
+    private static final Log LOGGER = LogFactory.getLog(StatusController.class);
     @GetMapping
     public String appRunning(){
 
-        log.info("-------------------appRunningContextPath");
+        LOGGER.info("-------------------appRunningContextPath");
         return "Contribution Service is Running...at root path";
     }
 
     @GetMapping
     @RequestMapping("cs")
     public String appRunningContextPath(){
-        log.info("-------------------appRunningContextPath");
+        LOGGER.info("-------------------appRunningContextPath");
         return "Contribution Service is Running...at context path";
     }
 }
