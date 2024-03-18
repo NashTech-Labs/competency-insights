a00da8e30439dbe346f98814774fcfc78e02b25a
shivamm31 <shivam.roy@knoldus.com>
2024-03-18 12:43:11 +0530

    added Add OKR

---
+++ contribution-service/src/main/java/com/nashtech/contributionservice/entity/OKRDataEntity.java
@@ -0,0 +1,28 @@
+package com.nashtech.contributionservice.entity;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.cloud.spring.data.firestore.Document;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collectionName = "okrData")
+public class OKRDataEntity {
+    @DocumentId
+    private String employeeId;
+    private String emailId;
+    private String activity;
+    private String radarTechnology;
+    private String title;
+    private String dueDate;
+    private String submissionDate;
+    private String link;
+    private String status;
+    private String description;
+
+}
