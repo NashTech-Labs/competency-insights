06c2dc8883bd24eee9af8d1c0493b00805bd0668
SushantGupta <sushant.gupta@knoldus.com>
2024-03-18 11:12:14 +0530

    updated contribution

--- contribution-service/src/main/java/com/nashtech/contributionservice/controller/StatusController.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/controller/StatusController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 @RestController
-@RequestMapping("/")
 public class StatusController {
 
     private static final Log LOGGER = LogFactory.getLog(StatusController.class);
     @GetMapping
     public String appRunning(){
 
-        LOGGER.info("-------------------appRunningContextPath");
+        LOGGER.info("-------------------appRunning  Root Path");
         return "Contribution Service is Running...at root path";
     }
 
     @GetMapping
-    @RequestMapping("cs")
+    @RequestMapping("/cs")
     public String appRunningContextPath(){
-        LOGGER.info("-------------------appRunningContextPath");
+        LOGGER.info("-------------------appRunning  ContextPath");
         return "Contribution Service is Running...at context path";
     }
+
 }
