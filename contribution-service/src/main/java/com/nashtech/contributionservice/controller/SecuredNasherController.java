309efe57b6f825b100c21f5d00a9e535dcbb64a7
SushantGupta <sushant.gupta@knoldus.com>
2024-03-20 13:43:30 +0530

    endpoints changes

--- contribution-service/src/main/java/com/nashtech/contributionservice/controller/SecuredNasherController.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/controller/SecuredNasherController.java
@@ -8,52 +8,47 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/cs")
 @AllArgsConstructor
-public class NasherController {
+public class SecuredNasherController {
     private final Processor processor;
     private final PasGo1Service pasGo1Service;
-    private static final Log log = LogFactory.getLog(NasherController.class);
 
-//    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
+    private static final Log log = LogFactory.getLog(SecuredNasherController.class);
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
     @GetMapping("/nashers")
     public Flux<Nasher> getAllNasher() {
         log.info("Enter into NasherController: Get all Nashers request");
         return processor.getNashers();
     }
 
-//    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
     @GetMapping("/nasher/{empId}")
     public Mono<Nasher> getNasherById(@PathVariable String empId) {
         log.info("Enter into NasherController: Get Nasher by employee Id: " + empId);
         return processor.getNasherInfo(empId);
     }
 
-//    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_user')")
     @GetMapping("/nasher/email/{email}")
     public Mono<Nasher> getNasherByEmail(@PathVariable String email) {
         log.info("Enter into NasherController: Get Nasher by email: " +  email);
         return processor.getNasherByEmail(email);
     }
 
-//    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
     @PostMapping("/nasher/save") // Temp basis to check firestore insertion
     public void saveNasher(@RequestBody Nasher nasher) {
         log.info("Enter into NasherController: Saving Nasher with Employee Id: " +  nasher.getEmpId());
         processor.saveNasher(nasher);
     }
 
-//    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
     @GetMapping("/trigger/pas")
     public String triggerPasService() {
         log.info("Enter into NasherController: Processing PAS request");
@@ -61,7 +56,7 @@ public class NasherController {
         return "Successfully triggered Pas service";
     }
 
-//    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
+    @PreAuthorize("hasAuthority('APPROLE_competency_insights_admin')")
     @GetMapping("/trigger/go1")
     public String triggerGo1Service() {
         log.info("Enter into NasherController: Processing GO1% request");
