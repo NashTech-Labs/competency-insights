50529597b4ea41427452b63cd38188053411cf5a
ankit-mogha <ankit.mogha@nashtechglobal.com>
2024-04-04 18:22:29 +0530

    Updated the Studio Page

--- competency-insights-ui/src/pages/AddOkr/AddOkrPage.js
+++ competency-insights-ui/src/pages/AddOkr/AddOkrPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormElement } from './components/FormElement';
 import { useNavigate } from "react-router-dom";
 import { PermanentDrawerLeft } from "../../components/Layout/NavBar";
@@ -9,11 +9,24 @@ export const AddOkrPage = ({name}) => {
   const [formData, setFormData] = useState({
     activity: '',
     radarTechnology: '',
+    competency: '',
     status:'Draft',
     title: '',
     dueDate: '',
     description: '',
   });
+
+  useEffect(() => {
+    const storedUserData = localStorage.getItem("userData");
+    if (storedUserData) {
+      const userData = JSON.parse(storedUserData);
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        competency: userData.department
+      }));
+    }
+  }, []);
+
   const addOkrPageUrl = `${process.env.REACT_APP_BACKEND_APP_URI}${process.env.REACT_APP_ADD_OKR_PAGE_URL}`;
 
   const handleSubmit = async (event) => {
