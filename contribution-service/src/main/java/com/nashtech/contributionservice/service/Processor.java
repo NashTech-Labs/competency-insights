583cf7624c93a0e946aa493c73644c1ffae7ecff
shivamm31 <shivam.roy@knoldus.com>
2024-04-26 13:05:18 +0530

    added code for in memory

--- contribution-service/src/main/java/com/nashtech/contributionservice/service/Processor.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/service/Processor.java
@@ -1,6 +1,7 @@
 package com.nashtech.contributionservice.service;
 
 import com.nashtech.contributionservice.entity.Nasher;
+import com.nashtech.contributionservice.entity.OKRDataEntity;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -11,4 +12,9 @@ public interface Processor {
     Mono<Nasher> getNasherInfo(String empId);
     Mono<Nasher> getNasherByEmail(String email);
      Flux<Nasher> getNashers();
+    void saveOKRData(OKRDataEntity okrData, String emailId, String name);
+    List<OKRDataEntity> getOKRData();
+    void deleteAllOKRData();
+    List<OKRDataEntity> getOKRDataByEmail(String email);
+    void updateOKRData(String emailId, String activity, String title, OKRDataEntity updatedData);
 }
--- contribution-service/src/main/java/com/nashtech/contributionservice/service/gcp/FirestoreProcessor.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/service/gcp/FirestoreProcessor.java
@@ -2,13 +2,18 @@ package com.nashtech.contributionservice.service.gcp;
 
 
 import com.nashtech.contributionservice.entity.Nasher;
+import com.nashtech.contributionservice.entity.OKRDataEntity;
 import com.nashtech.contributionservice.repo.FirestoreRepository;
+import com.nashtech.contributionservice.service.FirestoreService;
 import com.nashtech.contributionservice.service.Processor;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 @Service
@@ -17,6 +22,7 @@ import reactor.core.publisher.Mono;
 public class FirestoreProcessor implements Processor {
 
     private final FirestoreRepository firestoreRepository;
+    private final FirestoreService firestoreService;
 
     @Override
     public void saveNasher(Nasher info) {
@@ -35,5 +41,47 @@ public class FirestoreProcessor implements Processor {
     @Override
     public Flux<Nasher> getNashers() {
         return firestoreRepository.findAll();
+    }
+
+    @Override
+    public void saveOKRData(OKRDataEntity okrData, String emailId, String name) {
+        try {
+            firestoreService.saveOKRData(okrData, emailId, name);
+        } catch (ExecutionException | InterruptedException e) {
+        }
+    }
+
+    @Override
+    public List<OKRDataEntity> getOKRData() {
+        try {
+            return firestoreService.getOKRData();
+        } catch (ExecutionException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteAllOKRData() {
+        try {
+            firestoreService.deleteAllOKRData();
+        } catch (ExecutionException | InterruptedException e) {
+        }
+    }
+
+    @Override
+    public List<OKRDataEntity> getOKRDataByEmail(String email) {
+        try {
+            return firestoreService.getOKRDataByEmail(email);
+        } catch (ExecutionException | InterruptedException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void updateOKRData(String emailId, String activity, String title, OKRDataEntity updatedData) {
+        try {
+            firestoreService.updateOKRData(emailId, activity, title, updatedData);
+        } catch (ExecutionException | InterruptedException e) {
+        }
     }
 }
