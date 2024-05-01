d7bc4d9b5ae9e206f67ef9d8b3dfbf89ac94aca6
shivamm31 <shivam.roy@knoldus.com>
2024-05-01 12:20:49 +0530

    reformatted the code

--- contribution-service/src/main/java/com/nashtech/contributionservice/service/InMemoryProcessor.java
+++ contribution-service/src/main/java/com/nashtech/contributionservice/service/InMemoryProcessor.java
@@ -23,12 +23,12 @@ public class InMemoryProcessor implements Processor {
     @Override
     public void saveNasher(Nasher info) {
         log.info("Data saved in local [InMemory]");
-        inMemoryData.put(info.getEmpId(),info);
+        inMemoryData.put(info.getEmpId(), info);
     }
 
     @Override
     public Mono<Nasher> getNasherInfo(String empId) {
-        log.info("Retrieving Nasher data from local [InMemory] by Employee Id: {}",empId);
+        log.info("Retrieving Nasher data from local [InMemory] by Employee Id: {}", empId);
         if (inMemoryData.get(empId) == null) {
             throw new NasherNotFoundException(empId);
         }
@@ -42,6 +42,7 @@ public class InMemoryProcessor implements Processor {
                 .next()
                 .switchIfEmpty(Mono.error(new NasherNotFoundException("Nasher not found for email: " + email)));
     }
+
     @Override
     public Flux<Nasher> getNashers() {
         log.info("Retrieving Nasher data from local [InMemory]");
